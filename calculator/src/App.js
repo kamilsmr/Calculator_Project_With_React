ba44ba7e8c9f0269e7819f3f2a31500145b7e06f
kamilsmr <kamils6035@gmail.com>
2021-12-13 23:21:56 +0300

    button

---
+++ calculator/src/App.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+const App = () => {
+  const [result, setResult] = useState("hiii");
+  return;
+
+  <div className="container">
+    <form>
+      <input type="text" value={result} />
+    </form>
+    <div className="keypad">
+      <button></button>
+      <button></button>
+      <button></button>
+      <button></button>
+      <button></button>
+      <button></button>
+      <button></button>
+      <button></button>
+      <button></button>
+      <button></button>
+      <button></button>
+      <button></button>
+      <button></button>
+      <button></button>
+      <button></button>
+    </div>
+  </div>;
+};
+
+export default App;
